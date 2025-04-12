1b6c91be833659725bc02037bb2a92eaa01ee845
Prerna Arote <arote.prerna@gmail.com>
2025-04-12 08:42:22 +0530

    Create VulnerableVault.sol

---
+++ VulnerableVault.sol
@@ -0,0 +1,15 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidiy ^0.8.0; 
+Contract VulnerableVault{
+
+mapping(address => uint256) public balances;
+function deposit() public payable {
+    balances[msg.sender] +=msg.value;
+}
+
+function withdraw(uint256 amount) public {
+    require(balances[msg.sender]>=amount, "Insufficient balance");
+    (bool sent,) = msg.sender.call(value: amount, "");
+    require(sent, "failed to send the amount");
+    balances[msg.sender] -=amount;
+}
